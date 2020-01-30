5da0382e9f63387d86d918ddba0ded3f03f924a5
liliia-r <liliaglushko1992@gmail.com>
2020-01-30 23:25:21 +0200

    fixed 8

--- js/task-8.js
+++ js/task-8.js
@@ -4,7 +4,7 @@ const numberInput = document.querySelector("div#controls > input");
 const buttonRender = document.querySelector("button[data-action='render']");
 const buttonDestroy = document.querySelector("button[data-action='destroy']");
 const resultBoxes = document.querySelector("div#boxes");
-
+// let div;
 let generateColor = () => {
     return `rgb(${Math.floor(Math.random() * 255)},${Math.floor(
         Math.random() * 256)},${Math.floor(Math.random() * 256)})`;    
@@ -14,30 +14,28 @@ const createBoxes = () => {
     let amount = numberInput.value
     let width = 30;
     let height = 30;
+    const arr = [];
     for(let i = 0; i < amount; i += 1){
         const div = document.createElement("div");
-        insertBoxes()
+        arr.push(div);
   
         div.style.backgroundColor = generateColor()
 
         width += 10;
         height += 10;
         
-        resultBoxes.childNodes[i].style.width = width + "px"
-        resultBoxes.childNodes[i].style.height = height + "px"
-
-        resultBoxes.children[0].style.width = 30 + "px"
-        resultBoxes.children[0].style.height = 30 + "px"
+        div.style.width = width + "px"
+        div.style.height = height + "px"
     } 
-    return div;
+   return arr;  
 }
 
 const insertBoxes = () => {
-    resultBoxes.append(div);
+    resultBoxes.append(...createBoxes());
 }
 const destroyBoxes = () => {
     resultBoxes.innerHTML = "";
 }
 
-buttonRender.addEventListener("click", createBoxes)
+buttonRender.addEventListener("click", insertBoxes)
 buttonDestroy.addEventListener("click", destroyBoxes)
